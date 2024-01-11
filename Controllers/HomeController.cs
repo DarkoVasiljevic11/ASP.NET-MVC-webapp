44f1a544432ce70dd1038922efc63b41a7cbef33
DarkoVasiljevic11 <118727486+DarkoVasiljevic11@users.noreply.github.com>
2024-01-11 18:06:09 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UpisForme()
+        {
+            return View();
+        }
+            [HttpPost]
+            public ViewResult UpisForme(Forma ispis){
+                return View("ispis", ispis);
+            }
+        
+    }
+
+}
---
+++ Models/Forma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Forma
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
